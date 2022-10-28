241bec8295f8d9d0fd7534ac2ba37a5815e98a8c
N V Harish <harishnv1991@gmail.com>
2022-10-29 01:29:23 +0530

    generate response function added

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,18 @@ namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
 
-class Controller extends BaseController
-{
-    //
+class Controller extends BaseController {
+
+    protected function generateResponse($data = null, $status = Response::HTTP_OK, $headers = array()) {
+        if ($status != Response::HTTP_OK && $status != Response::HTTP_CREATED) {
+            $data['status'] = 'ERROR';
+            $data['message'] = $data['message'] ?? Response::$statusTexts[$status];
+        }
+
+        if (is_null($data)) {
+            return response($data, $status, $headers)->header('Content-Type', 'application/json');
+        }
+        return response()->json($data, $status, $headers);
+    }
+
 }
