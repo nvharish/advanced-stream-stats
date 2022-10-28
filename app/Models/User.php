590333119d1d0b021a1135cef4e43a107e050a43
N V Harish <harishnv1991@gmail.com>
2022-10-29 02:17:21 +0530

    jwt token generate bug fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Lumen\Auth\Authorizable;
+use App\Services\Service;
+use Illuminate\Support\Facades\DB;
+use Firebase\JWT\JWT;
+use Throwable;
 
 class User extends Model implements AuthenticatableContract {
 
