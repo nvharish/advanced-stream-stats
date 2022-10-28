115fe39223bd79e715ea120e9b2566c688a38b3e
N V Harish <harishnv1991@gmail.com>
2022-10-29 02:19:01 +0530

    json response bug fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Symfony\Component\HttpFoundation\Response;
 
 class Controller extends BaseController {
 
